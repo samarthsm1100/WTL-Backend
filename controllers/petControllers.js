192d1c74c1427515b8104a872249f1a9b2b57db4
Samarth <samarthmali1100@gmail.com>
2024-01-16 14:54:28 +0530

    petroutes updated

--- controllers/petControllers.js
+++ controllers/petControllers.js
@@ -5,12 +5,12 @@ const postFoundPet = async (req, res) => {
     if(!userId){
         return res.status(400).json({error : "User not Authenticated"})
     }
-    const {species, breed, color, location, status, description} = req.body;
+    const {species, breed, color, address, status, description} = req.body;
     const pet = new Pet({
         species,
         breed,
         color,
-        location,
+        address,
         status,
         description,
         reportedBy: userId
@@ -19,12 +19,34 @@ const postFoundPet = async (req, res) => {
         pet.image_url = req.file.path
     }
     try {
-        // const newPet = await pet.save();
-        console.log(pet)
-        res.status(201).json(pet);
+        const newPet = await pet.save();
+        // console.log(pet)
+        res.status(201).json(newPet);
     } catch (error) {
         res.status(409).json({message: error.message});
     }
 }
 
-module.exports = {postFoundPet}
+const getAllPets = async(req,res) => {
+    try{
+        const petData = await Pet.find({});
+        console.log(petData)
+        res.status(200).json(petData);
+    }
+    catch(err){
+        res.status(404).json({message: err.message});
+    }
+}
+
+const getPetByID = async(req,res) => {
+    try{
+        const {_id} = req.body;
+        const petData = await Pet.findById(_id);
+        res.status(200).json(petData);
+    }
+    catch(err){
+        res.status(404).json({message: err.message});
+    }
+}
+
+module.exports = {postFoundPet, getAllPets, getPetByID}
--- routes/petRoutes.js
+++ routes/petRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const Pet = require('../models/petModel');
 const { validateToken } = require('../middleware/requireAuth');
-const { postFoundPet } = require('../controllers/petControllers');
+const { postFoundPet, getAllPets, getPetByID } = require('../controllers/petControllers');
 
 const router = express.Router();
 const upload = require('../middleware/upload');
 
 router.post('/foundpet', validateToken, upload.single('image_url'), postFoundPet );
+router.get('/pet', validateToken, getPetByID);
+router.get('/allpets', validateToken, getAllPets);
 
 module.exports = router;
