f92c754bfe57891b73afce26b76d5bb60f6ec90b
Samarth <samarthmali1100@gmail.com>
2024-02-16 20:44:37 +0530

    BACKEND

--- middleware/upload.js
+++ middleware/upload.js
@@ -11,7 +11,7 @@ var storage = multer.diskStorage({
 })
 
 var upload = multer({ 
-    storage: storage, 
+    storage: storage,   
     fileFilter: function (req, file, cb) {
         if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
             return cb(new Error('File format not supported'))
