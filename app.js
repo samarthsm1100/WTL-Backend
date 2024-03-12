ee761f5bb9bfee82b09b82e25065611f5ec150bc
Samarth <samarthmali1100@gmail.com>
2024-03-12 23:40:02 +0530

    cors-revert :)

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 
 // middleware
 app.use(cors({
-   origin:"*",
+   origin:["http://localhost:5173", "http://127.0.0.1:5173"],
    credentials:true
 }));
 app.use(express.json());
